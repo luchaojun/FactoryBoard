1378dfca3e3e13539bf0f6bea64c6a215aab4ef4
Chaojun_Lu <Chaojun_Lu@clevo.com.tw>
2024-08-30 17:02:02 +0800

    加入小圆圈的使用
    小圆圈

--- pages/workorder/workorder.js
+++ pages/workorder/workorder.js
@@ -5,7 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      listData:[
+        {name: "test1", content1: "con1", content2: "con1", content3: "con1", content4: "con1"},
+        {name: "test2", content1: "con2", content2: "con2", content3: "con2", content4: "con2"},
+        {name: "test3", content1: "con3", content2: "con3", content3: "con3", content4: "con3"},
+        {name: "test4", content1: "con4", content2: "con4", content3: "con4", content4: "con4"},
+        {name: "test5", content1: "con5", content2: "con5", content3: "con5", content4: "con5"},
+        {name: "test6", content1: "con6", content2: "con6", content3: "con6", content4: "con6"},
+        {name: "test7", content1: "con7", content2: "con7", content3: "con7", content4: "con7"},
+        {name: "test8", content1: "con8", content2: "con8", content3: "con8", content4: "con8"},
+        {name: "test9", content1: "con9", content2: "con9", content3: "con9", content4: "con9"},
+        {name: "test10", content1: "con10", content2: "con10", content3: "con10", content4: "co10"}
+      ],
   },
 
   /**

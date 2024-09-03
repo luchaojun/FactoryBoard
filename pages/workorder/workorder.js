b41d14de6efd17e0898bc3198c7bbdeb040386d6
Chaojun_Lu <Chaojun_Lu@clevo.com.tw>
2024-09-03 17:04:37 +0800

    Modify
    Add the test progress

--- pages/workorder/workorder.js
+++ pages/workorder/workorder.js
@@ -5,21 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    tasks: [
-      {
-        title: '任务1',
-        author: '作者1',
-        description: '这是任务1的描述',
-        progress: 50,
-      },
-      {
-        title: '任务2',
-        author: '作者2',
-        description: '这是任务2的描述',
-        progress: 75,
-      },
-      // ...其他任务数据
-    ],
+   
   },
 
   /**
@@ -38,37 +24,49 @@ Page({
   },
 
   drawProgressCircles: function () {
-    for (let i = 0; i < this.data.tasks.length; i++) {
-        const task = this.data.tasks[i];
+    for (let i = 0; i < this.data.process.length; i++) {
+        const task = this.data.process[i];
+        const p = Math.ceil(task["PRODUCT_QUANTITY"] / task["QUANTITY"] * 100);
+        task["progress"] = p;
         const ctx = wx.createCanvasContext('progressCanvas' + i);
         const centerX = 35;
         const centerY = 55;
         const radius = 30;
         const startAngle = -Math.PI / 2;
-        const endAngle = (task.progress * Math.PI * 2) / 100 + startAngle;
+        const endAngle = (p * Math.PI * 2) / 100 + startAngle;
   
-        // 绘制背景圆环
-        // ctx.beginPath();
-        // ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
-        // ctx.setFillStyle('#999');
-        // ctx.fill();
-
         // 绘制进度圆环
         ctx.beginPath();
         ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
         ctx.setLineWidth(4);
         ctx.setStrokeStyle('#999');
         ctx.stroke();
-  
+
         // 绘制进度圆环
         ctx.beginPath();
         ctx.arc(centerX, centerY, radius, startAngle, endAngle);
         ctx.setLineWidth(4);
-        ctx.setStrokeStyle('#00f');
+        if(p == 100){
+          ctx.setStrokeStyle('#0f0');
+        }else{
+          ctx.setStrokeStyle('#00f');
+        }
         ctx.stroke();
-  
         ctx.draw();
     }
+    this.setData({process: this.data.process})
+  },
+
+  findWorkOrders:function(event){
+    console.log("findWorkOrders");
+  },
+
+  sortWorkOrders:function(event){
+    console.log("sortWorkOrders");
+  },
+
+  selectWorkOrders:function(event){
+    console.log("selectWorkOrders");
   },
 
   /**
